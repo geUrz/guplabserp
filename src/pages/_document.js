700eed61b7d087833512529146a640a2dda2b15e
geUrz <gerardourzua20@gmail.com>
2026-01-08 21:13:20 -0800

    start2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#000000" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <link rel="apple-touch-icon" href="/icons/control-192x192.png" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
       <body>
